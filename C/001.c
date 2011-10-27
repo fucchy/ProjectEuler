5c19c5b021b1927708871b61130b1ac96747ec10
john <john@john-desktop.(none)>
2011-10-27 20:44:20 +0900

    create directory of C

---
+++ C/001.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(int argc, char* argv[]){
+	int x;
+	int y=1000;
+	int sum=0;
+	for(x=1;x<y;x++){
+		if(x%3==0||x%5==0){
+			sum=sum+x;
+		}
+	}
+	printf("%d\n",sum);
+	return 0;
+}
+
