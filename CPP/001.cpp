d6096436b983a346a78001f84be9465e14a1c411
john <john@john-desktop.(none)>
2011-12-28 11:33:19 +0900

    rewrite 001.cpp

---
+++ CPP/001.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<time.h>
+
+int main(int argc,char* argv[]){
+    clock_t start,end;
+	start=clock();
+
+	int i=0;
+	int sum=0;
+	while(i<1000){
+	    if(i%3==0||i%5==0){
+			sum+=i;
+		}
+		i++;
+	}
+	std::cout<<sum<<std::endl;
+
+	end=clock();
+	std::cout<<"time:"<<end-start<<std::endl;
+
+	return 0;
+}
+
