32b7cc87ce154fe427dc6dde4cd7df284c511fb0
john <john@john-desktop.(none)>
2011-12-29 10:23:22 +0900

    rewrite 007.cpp

--- CPP/007.cpp
+++ CPP/007.cpp
@@ -5,36 +5,36 @@ int nth_Prime(int n);
 
 int main(int argc,char* argv[]){
     clock_t start,end;
-	start=clock();
-	int n=10001;
+    start=clock();
+    int n=10001;
 
-	std::cout<<nth_Prime(n)<<std::endl;
+    std::cout<<nth_Prime(n)<<std::endl;
 
-	end=clock();
-	std::cout<<"time:"<<(end-start)/1000<<"ms"<<std::endl;
+    end=clock();
+    std::cout<<"time:"<<(end-start)/1000<<"ms"<<std::endl;
 
-	return 0;
+    return 0;
 }
 
 // n番目の素数を返すnth_Prime()関数
 int nth_Prime(int n){
     int a=1;
-	int b=2;
-	while(1){
-	    int i=2;
-		while(i<=b){
-		    if(i==b){
-			    if(a==n){
-				    return b;
-			    }
-				a++;
-			}
-			if(b%i==0){
-				break;
-			}
-			i++;
-		}
-		b++;
-	}
+    int b=2;
+    while(1){
+        int i=2;
+        while(i<=b){
+            if(i==b){
+                if(a==n){
+                    return b;
+                }
+                a++;
+            }
+            if(b%i==0){
+                break;
+            }
+            i++;
+        }
+        b++;
+    }
 }
 
