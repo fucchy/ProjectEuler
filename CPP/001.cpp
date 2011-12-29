0cdfe473f6dac6660b60884d8100b93c730d4e89
john <john@john-desktop.(none)>
2011-12-29 23:36:51 +0900

    rewrite 001.cpp

--- CPP/001.cpp
+++ CPP/001.cpp
@@ -3,21 +3,21 @@
 
 int main(int argc,char* argv[]){
     clock_t start,end;
-	start=clock();
+    start=clock();
 
-	int i=0;
-	int sum=0;
-	while(i<1000){
-	    if(i%3==0||i%5==0){
-			sum+=i;
-		}
-		i++;
-	}
-	std::cout<<sum<<std::endl;
+    int i=0;
+    int sum=0;
+    while(i<1000){
+        if(i%3==0||i%5==0){
+            sum+=i;
+        }
+        i++;
+    }
+    std::cout<<sum<<std::endl;
 
-	end=clock();
-	std::cout<<"time:"<<(double)(end-start)/CLOCKS_PER_SEC<<"ms"<<std::endl;
+    end=clock();
+    std::cout<<"time:"<<(double)(end-start)/CLOCKS_PER_SEC<<"ms"<<std::endl;
 
-	return 0;
+    return 0;
 }
 
