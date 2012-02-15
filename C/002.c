e1eb99c55ba3846f1d2be928c0522d43a5ac5bea
john <john@john-desktop.(none)>
2012-02-15 12:24:27 +0900

    rewrite 002.c

--- C/002.c
+++ C/002.c
@@ -4,24 +4,24 @@
 int fibonacci(int x);
 
 int main(int argc,char *argv[]){
-	int i;
-	int sum=0;
+  int i;
+  int sum=0;
 
-	for(i=1;fibonacci(i)<4000000;i++){
-		if(fibonacci(i)%2==0){
-			sum=sum+fibonacci(i);
-		}
-	}
-	printf("%d\n",sum);	
-	return EXIT_SUCCESS;
+  for(i=1;fibonacci(i)<4000000;i++){
+    if(fibonacci(i)%2==0){
+      sum=sum+fibonacci(i);
+    }
+  }
+  printf("%d\n",sum);
+  return EXIT_SUCCESS;
 }
 
 int fibonacci(int x){
-	if(x>=3){
-		return fibonacci(x-1)+fibonacci(x-2);
-	}else if(x==2){
-		return 2;
-	}else if(x==1){
-		return 1;
-	}
+  if(x>=3){
+    return fibonacci(x-1)+fibonacci(x-2);
+  }else if(x==2){
+    return 2;
+  }else if(x==1){
+    return 1;
+  }
 }
