98b3de1d221b6fbbeec8bb4569d30fede794f872
john <john@john-desktop.(none)>
2012-02-15 12:25:36 +0900

    rewrite 001.c

--- C/001.c
+++ C/001.c
@@ -2,15 +2,15 @@
 #include<stdlib.h>
 
 int main(int argc,char *argv[]){
-	int x;
-	int y=1000;
-	int sum=0;
-	for(x=1;x<y;x++){
-		if(x%3==0||x%5==0){
-			sum=sum+x;
-		}
-	}
-	printf("%d\n",sum);
-	return EXIT_SUCCESS;
+  int x;
+  int y=1000;
+  int sum=0;
+  for(x=1;x<y;x++){
+    if(x%3==0||x%5==0){
+      sum=sum+x;
+    }
+  }
+  printf("%d\n",sum);
+  return EXIT_SUCCESS;
 }
 
